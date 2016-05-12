55de14895bb936f9e45ff93cb7bceeaa6803238f
fcuenca4 <kero4ever@gmail.com>
2016-05-12 00:29:51 -0300

    new routing

--- src/MiAppBundle/Controller/DefaultController.php
+++ src/MiAppBundle/Controller/DefaultController.php
@@ -19,10 +19,10 @@ class DefaultController extends Controller
         } else {
             $usr = $this->container->get('security.token_storage')->getToken()->getUser();
             if ($usr->hasRole('ROLE_ADMIN')) {
-                $response = $this->render('C:\Users\fcuenca\Desktop\proyecto_symfony\src\MiAppBundle\Resources\views\adminview.html.twig');
+                return $this->RedirectToRoute('admin_index');
             } else if ($usr->hasRole('ROLE_USER')) {
                 {
-                    $response = $this->render('C:\Users\fcuenca\Desktop\proyecto_symfony\src\MiAppBundle\Resources\views\editorview.html.twig');
+                    return $this->RedirectToRoute('editor_index');
                 }
             }
         }
